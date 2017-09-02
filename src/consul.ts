b90a14f5ff5a39055013ce076ecdbd760ec3f388
Uncle Charlie <lixiangdong@mobike.com>
2017-09-02 23:21:34 +0800

    add & delete files

---
+++ src/consul.ts
@@ -0,0 +1,22 @@
+import papi from 'papi';
+import util from 'util';
+
+import { Acl } from './acl';
+import { Agent } from './agent';
+import { Catalog } from './catalog';
+import { Event } from './event';
+import { Health } from './health';
+import { Kv } from './kv';
+import { Lock } from './lock';
+import { Query } from './query';
+import { Session } from './session';
+import { Status } from './status';
+import { Watch } from './watch';
+import constants from './constants';
+import utils from './utils';
+
+// class Consul extends papi.Client {
+//   constructor(opts) {
+
+//   }
+// }
